a18fa4d707cfed6acc5240f9ca16d648cd4e094b
JaimeRomero <jaime_im@outlook.com>
2022-04-21 18:01:21 -0500

    Se agrego la prueba getInfo para hacerla fallar

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,11 +1,8 @@
 const spiderman = require('./../app/spiderman')
 describe("Unit test spiderman class", () => {
-    test('Test 1: Create an spiderman object', () => {
-      const andrewGarfield = new spiderman("Spiderman Sony", 31, "Andrew Garfield", 31, "Sony")
-      expect(andrewGarfield.name).toBe("Spiderman Sony");
-      expect(andrewGarfield.age).toBe(31);
-      expect(andrewGarfield.actor).toBe("Andrew Garfield");
-      expect(andrewGarfield.movies).toBe(31);
-      expect(andrewGarfield.studio).toBe("Sony");
+    test('Test 2: Use the method getInfo()', () => {
+      const tomHolland = new spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+      expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio");
+      
     });
   })
