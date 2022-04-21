636f68509f48247b7728048b9434655d609d252a
JaimeRomero <jaime_im@outlook.com>
2022-04-21 15:51:34 -0500

    Se modifico para el archivo para hacer hacer valida  la prueba de la clase MissionComander

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -2,7 +2,7 @@ const MissionCommander = require('./../app/missionCommander');
 describe("Unit Tests for Mission Commander Class", () => {
     test('Test 1: Unit Tests for Mission Commander Class', () => {
       const myMissionCommander = new MissionCommander("Woopa") 
-      expect(myMissionCommander.name).toBe("Woopasdfsdf");
+      expect(myMissionCommander.name).toBe("Woopa");
     });
   })
   
