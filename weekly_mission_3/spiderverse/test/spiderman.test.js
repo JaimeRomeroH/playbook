37ac42e5b9cbd9b708cf7e4b2f3b73a66f5d63ad
JaimeRomero <jaime_im@outlook.com>
2022-04-21 18:33:02 -0500

    Se modifico el archivo spiderman.test.js para probar el metodo getInfo()

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,8 +1,8 @@
 const spiderman = require('./../app/spiderman')
 describe("Unit test spiderman class", () => {
     test('Test 2: Use the method getInfo()', () => {
-      const tomHolland = new spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
-      expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio");
+      const tomHolland = new spiderman()
+      expect(tomHolland.getInfo("Tom Holland", "Marvel")).toBe("Hey, I'm Tom Holland from Marvel studio");
       
     });
   })
