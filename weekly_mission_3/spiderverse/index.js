0428dabead595414de01c6c69a18251fbaa2d7c7
JaimeRomero <jaime_im@outlook.com>
2022-04-21 18:38:25 -0500

    Se agrego el objeto tomHolland al archivo index.js

--- weekly_mission_3/spiderverse/index.js
+++ weekly_mission_3/spiderverse/index.js
@@ -1,7 +1,9 @@
 const spiderman = require('./app/spiderman');
 const andrewGarfield = new spiderman("Spiderman Sony", 31, "Andrew Garfield", 31, "Sony")
+const tomHolland = new spiderman()
 console.log(andrewGarfield.name)
 console.log(andrewGarfield.age)
 console.log(andrewGarfield.actor)
 console.log(andrewGarfield.movies)
 console.log(andrewGarfield.studio)
+console.log(tomHolland.getInfo("Tom Holland", "Marvel"))
