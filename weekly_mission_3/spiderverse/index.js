8e17441355fe71a490879982bb22cdb1942dc75b
JaimeRomero <jaime_im@outlook.com>
2022-04-21 17:41:23 -0500

    Se agrego codigo al archivo index.js para importar la clase spiderman y crear el objeto

---
+++ weekly_mission_3/spiderverse/index.js
@@ -0,0 +1,7 @@
+const spiderman = require('./app/spiderman');
+const andrewGarfield = new spiderman("Spiderman Sony", 31, "Andrew Garfield", 31, "Sony")
+console.log(andrewGarfield.name)
+console.log(andrewGarfield.age)
+console.log(andrewGarfield.actor)
+console.log(andrewGarfield.movies)
+console.log(andrewGarfield.studio)
