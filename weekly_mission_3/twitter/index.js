96e7ad4043feb47ad8d41215fd9193725484517a
JaimeRomero <jaime_im@outlook.com>
2022-04-23 00:30:10 -0500

    Se agrego el codigo validado de los getters en index.js

--- weekly_mission_3/twitter/index.js
+++ weekly_mission_3/twitter/index.js
@@ -1,3 +1,9 @@
 const User = require('./app/models/User');
 const user = new User(1, "JaimeRH", "Jaime", "Hola", "", "")
 console.log(user)
+console.log(user.getid)
+console.log(user.getusername)
+console.log(user.getname)
+console.log(user.getbio)
+console.log(user.dateCreated)
+console.log(user.getlastUpdated)
