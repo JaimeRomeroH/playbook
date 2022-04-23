e5645b88566425c36bcddfec79c5c1ec52e39c00
JaimeRomero <jaime_im@outlook.com>
2022-04-22 21:48:06 -0500

    Se creo la prueba para pasar dateCreated y lastUpdated como no indefinidos

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -3,15 +3,15 @@ const User = require('./../../app/models/User')
 describe("Unit test for user class", () =>{
     test("Create an user object", () =>{
         //Funcion a probar
-        const user = new User(1, "JaimeRH", "Jaime", "Hola", "22-04-2022", "22-04-2022")
+        const user = new User(1, "JaimeRH", "Jaime", "Hola", "", "")
 
         //Validacion de resultados
         expect(user.id).toBe(1)
         expect(user.username).toBe("JaimeRH")
         expect(user.name).toBe("Jaime")
         expect(user.bio).toBe("Hola")
-        expect(user.dateCreated).toBe("22-04-2022")
-        expect(user.lastUpdated).toBe("22-04-2022")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 
 })
