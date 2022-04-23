4ae628e7d8d84bcfc125555afa62067fb17b9623
JaimeRomero <jaime_im@outlook.com>
2022-04-22 22:09:10 -0500

    Se agrego el codigo probado con dateCreated y lastUpdated

--- weekly_mission_3/twitter/index.js
+++ weekly_mission_3/twitter/index.js
@@ -1,3 +1,3 @@
 const User = require('./app/models/User');
-const user = new User(1, "JaimeRH", "Jaime", "Hola", "22-04-2022", "22-04-2022")
+const user = new User(1, "JaimeRH", "Jaime", "Hola", "", "")
 console.log(user)
