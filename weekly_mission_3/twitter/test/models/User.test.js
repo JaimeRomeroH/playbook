16e199043c931bd5ab991da6d75b0177960d32b3
JaimeRomero <jaime_im@outlook.com>
2022-04-23 01:34:56 -0500

    Se validaron los setters

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,17 +1,17 @@
 const User = require('./../../app/models/User')
 
 describe("Unit test for user class", () =>{
-    test("Add getters", () =>{
+    test("Add setters", () =>{
         //Funcion a probar
         const user = new User(1, "JaimeRH", "Jaime", "Hola", "", "")
 
         //Validacion de resultados
-        expect(user.getid).toBe(1)
-        expect(user.getusername).toBe("JaimeRH")
-        expect(user.getname).toBe("Jaime")
-        expect(user.getbio).toBe("Hola")
-        expect(user.getdateCreated).not.toBeUndefined()
-        expect(user.getlastUpdated).not.toBeUndefined()
+        user.setusername = "JaimeAd"
+        expect(user.username).toBe("JaimeAd")
+        user.setbio = "New bio"
+        expect(user.bio).toBe("New bio")
+        
+       
     });
 
 })
