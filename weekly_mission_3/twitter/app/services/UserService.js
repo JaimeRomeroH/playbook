e3e6a8fc271c7a351f89ba6b10f1db8fbb799b94
JaimeRomero <jaime_im@outlook.com>
2022-04-26 23:05:25 -0500

    Se creo el metodo getInfo

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -1,7 +1,12 @@
 const User = require('./../models/User')
 class UserService{
     static create(id, username, name){
-     return new User(id, username, name, "Sin bio")
+      return new User(id, username, name, "Sin bio")
+    }
+    static getInfo(user){
+    
+      
+      return [user.id, user.username, user.name, user.bio]  
     }
 }
 
