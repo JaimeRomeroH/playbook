e0ff7e220421cebdab56a90de0ce5f0d7ffcec80
JaimeRomero <jaime_im@outlook.com>
2022-04-26 23:06:27 -0500

    Se valido el funcionamiento correcto de el metodo getInfo

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -1,3 +1,4 @@
+
 const UserService = require('./../../app/services/UserService')
 describe("Test for UserService",()=>{
    test("1.Create a new user using the UserService",()=>{ 
@@ -6,5 +7,14 @@ describe("Test for UserService",()=>{
     expect(user.name).toBe("Jaime")
     expect(user.id).toBe(1)
     expect(user.bio).not.toBeUndefined()
-})
+   })
+
+   test("2.Get all user data in a list",()=>{ 
+    const user = UserService.create(1, "JaimeRH", "Jaime")
+    const userInfoInList = UserService.getInfo(user)
+    expect(userInfoInList[0]).toBe(1)
+    expect(userInfoInList[1]).toBe("JaimeRH")
+    expect(userInfoInList[2]).toBe("Jaime")
+    expect(userInfoInList[3]).toBe("Sin bio")
+   })
 })
