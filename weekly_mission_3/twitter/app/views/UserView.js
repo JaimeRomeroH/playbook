f928a52a418108c9339ed098e2c66f1c83fcff0e
JaimeRomero <jaime_im@outlook.com>
2022-04-28 18:29:10 -0500

    Se creo el metodo createUser

---
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -0,0 +1,11 @@
+const User = require('./../models/User')
+const UserService = require('./../services/UserService')
+class UserView{
+    static createUser(payload){
+        if (payload === null){
+            return  {error: "payload no existe"}
+        }
+    }
+}
+
+module.exports = UserView;
