d4f1677642c5c75806fe5961d5a826479b8df6fe
JaimeRomero <jaime_im@outlook.com>
2022-04-29 18:02:34 -0500

    Se valido el correcto funcionamiento de la condicion de que todos los valores sean correctos

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -10,5 +10,25 @@ describe("Test for UserView", () => {
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesita tener un valor válido/)
+
+    
     })
+
+    test('Return an error object when try to create a new user with payload with missing properties', () =>{
+    const payload = {username: "username"}
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesita tener un valor válido/)
+
+    
+    })
+
+    test('Create an user by a given valid payload', () =>{
+        const payload = {username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
+    
+        
+        })
 })
