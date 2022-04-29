cc175b17d80a1ebcc40256639053434cb8a51167
JaimeRomero <jaime_im@outlook.com>
2022-04-29 02:52:57 -0500

    Se agreo la condicion para evaluar si existen valores invalidos

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -5,6 +5,11 @@ class UserView{
         if (payload === null){
             return  {error: "payload no existe"}
         }
+        else if((payload.username === null || typeof(payload.username) !== 'string') || (payload.name === null || typeof(payload.name) !== 'string') ||  (payload.id === null || 
+           typeof(payload.id) !== 'number')){
+            return {error: "necesita tener un valor válido"}
+        }
+        
     }
 }
 
