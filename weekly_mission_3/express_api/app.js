a0f686b1502fd9ea163034df0d79e03b8306c2b0
JaimeRomero <jaime_im@outlook.com>
2022-05-01 22:47:32 -0500

    Se agrego el metodo POST y PUT al archivo app.js

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -24,6 +24,22 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api explorers POST request ${new Date()}`)
+    const requestBody = req.body
+    
+    res.status(201).json({message: "Created"})
+})
+
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body
+    
+    res.status(200).json({message: "Updated"})
+})
+
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
