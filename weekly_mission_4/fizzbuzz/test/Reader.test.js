0c61cea1fc5469608997a89c5c7d75ce0bc2b69f
JaimeRomero <jaime_im@outlook.com>
2022-05-04 17:23:33 -0500

    Se valido el correcto funcionamiento de la clase Reader

---
+++ weekly_mission_4/fizzbuzz/test/Reader.test.js
@@ -0,0 +1,12 @@
+const Reader = require('./../Reader');
+
+describe("Unit Tests for Reader Class", () => {
+    test('Test 1: Unit Tests for Reader Class', () => {
+     
+      const file = 'explorers.json'
+      
+      const reading = Reader.readJsonFile(file) 
+      const data = require('./../explorers.json')
+      expect(reading).toStrictEqual(data)
+    });
+  })
