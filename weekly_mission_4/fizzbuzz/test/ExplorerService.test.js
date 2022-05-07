5d232f6edd5a775624996326e485fd6958649fc4
JaimeRomero <jaime_im@outlook.com>
2022-05-06 21:45:41 -0500

    Se valido el correcto funcionamiento del metodo filterByMission

--- weekly_mission_4/fizzbuzz/test/ExplorerService.test.js
+++ weekly_mission_4/fizzbuzz/test/ExplorerService.test.js
@@ -6,8 +6,9 @@ describe("Unit Tests for ExplorerService Class", () => {
 
      const explorers = Reader.readJsonFile("explorers.json"); 
      const explorerMission = ExplorerService.filterByMission(explorers, "node")
-     const array = ["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"]
-     expect(explorerMission).toStrictEqual(array)
+     const arrayNode = ["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"]
+     const arrayJava = ["ajolonauta6", "ajolonauta7", "ajolonauta8", "ajolonauta9", "ajolonauta10"]
+     expect(explorerMission).toStrictEqual(arrayNode)
     });
 
     test('Test 2: Unit Tests for ExplorerService Class', () => {
