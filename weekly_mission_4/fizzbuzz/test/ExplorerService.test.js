133c7136c5db996be82869cc5da3d4fc4712806f
JaimeRomero <jaime_im@outlook.com>
2022-05-07 19:23:17 -0500

    Se valido el correcto funcionamiento de el metodo  getExplorersUserNamesByMission

--- weekly_mission_4/fizzbuzz/test/ExplorerService.test.js
+++ weekly_mission_4/fizzbuzz/test/ExplorerService.test.js
@@ -16,7 +16,15 @@ describe("Unit Tests for ExplorerService Class", () => {
        const explorerMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
       
        expect(explorerMission).toBe(10)
-      })    
+      }) 
+      
+      test('Test 3: Unit Tests for ExplorerService Class', () => {
+  
+        const explorers = Reader.readJsonFile("explorers.json"); 
+        const explorerMission = ExplorerService.getExplorersUserNamesByMission(explorers, "node")
+       
+        expect(explorerMission).toStrictEqual(ExplorerService.getExplorersUserNamesByMission(explorers, "node"))
+       })    
  
 
     
